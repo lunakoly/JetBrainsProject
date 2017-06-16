a4de85db7742bd1d5fa9d38f99b116140f352128
Serg1k24 <se2620@yandex.ru>
2017-06-16 14:49:28 +0300

    + Main menu skeleton

--- app/src/main/java/ru/luna_koly/jetbrainsproject/MainActivity.java
+++ app/src/main/java/ru/luna_koly/jetbrainsproject/MainActivity.java
@@ -1,13 +1,13 @@
 package ru.luna_koly.jetbrainsproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import ru.luna_koly.jetbrainsproject.activityTemplates.NoTitleBarActivity;
+
+public class MainActivity extends NoTitleBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/ru/luna_koly/jetbrainsproject/activityTemplates/NoTitleBarActivity.java
@@ -0,0 +1,21 @@
+package ru.luna_koly.jetbrainsproject.activityTemplates;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import ru.luna_koly.jetbrainsproject.R;
+
+/**
+ * Created by user on 6/16/17.
+ */
+
+public class NoTitleBarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Hide title bar
+        setContentView(R.layout.activity_main);
+    }
+}
